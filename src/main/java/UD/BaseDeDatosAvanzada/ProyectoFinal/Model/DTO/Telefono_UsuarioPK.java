7865c4a4828c696b20812586a8e24bf0d2117e6b
J4F3ET <jslopezz@udistrital.edu.co>
2023-05-20 17:40:30 -0500

    Agregando CRUD de UsuarioDTO

--- src/main/java/UD/BaseDeDatosAvanzada/ProyectoFinal/Model/DTO/Telefono_UsuarioPK.java
+++ src/main/java/UD/BaseDeDatosAvanzada/ProyectoFinal/Model/DTO/Telefono_UsuarioPK.java
@@ -1,5 +1,6 @@
 package UD.BaseDeDatosAvanzada.ProyectoFinal.Model.DTO;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -9,6 +10,7 @@ import java.util.Objects;
  * @Column indica que el atributo es un campo de la tabla
  * @ManyToOne indica la relación muchos a uno
  * @JoinColumn indica el nombre de la columna que hace referencia a la clave primaria de la tabla referenciada
+ * @JsonBackReference indica que la clase es una relación muchos a uno
  * Clase que representa la llave primaria de la tabla Telefono_Usuario de la base de datos.
  */
 @Embeddable
@@ -18,6 +20,7 @@ public class Telefono_UsuarioPK implements Serializable {
     private long telefono;
     @ManyToOne(fetch = FetchType.LAZY,targetEntity = UsuarioDTO.class)
     @JoinColumn(name = "id_usuario", referencedColumnName = "id_usuario")
+    @JsonBackReference
     private UsuarioDTO usuario;
     public Telefono_UsuarioPK() {
     }
